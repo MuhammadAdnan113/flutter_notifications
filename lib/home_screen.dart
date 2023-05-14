9a24d08ae9c4c56829976c7413fa95aa7048d85f
Muhammad Adnan <adnan50351@gmail.com>
2023-05-14 18:08:08 +0500

    When App is Killed or background

---
+++ lib/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notifications/notification_services.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  NotificationServices notificationServices = NotificationServices();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    notificationServices.requestNotificationPermission();
+    notificationServices.firebaseInit();
+    //notificationServices.isTokenRefresh();
+    notificationServices.getDeviceToken().then((value) {
+      debugPrint("Device token: ");
+      debugPrint(value);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Test'),
+      ),
+    );
+  }
+}
